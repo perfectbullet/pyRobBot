a4be832bdb30bf796cfde8df7ae5ee7a312f01f8
zj <zhoujing382@gmail.com>
2024-09-13 10:33:57 +0800

    修改为音频IO

--- pyrobbot/whisper_service_utils.py
+++ pyrobbot/whisper_service_utils.py
@@ -1,12 +1,14 @@
-import os
+import io
+
 import requests
+from loguru import logger
 
 whisper_base_url = 'http://localhost:9000/asr'
 
 
-def transcribe(file_path):
+def transcribe(wav_buffer):
     '''
-
+    音频 io 转文本
     '''
 
     headers = {
@@ -23,15 +25,21 @@ def transcribe(file_path):
         'word_timestamps': 'false',
         'output': 'txt',
     }
-    fname = os.path.basename(file_path)
+
+    fname = wav_buffer.name
     files = {
-        'audio_file': (fname, open(file_path, 'rb'), 'audio/wav'),
+        'audio_file': (fname, wav_buffer, 'audio/wav'),
     }
     response = requests.post(whisper_base_url, params=params, headers=headers, files=files)
-    return response.content.decode('utf-8')
+    txt = response.content.decode('utf-8')
+    logger.info('whisper result is {}'.format(txt))
+    return txt
 
 
 if __name__ == '__main__':
-    file_path = '../tests/你不要让我做饭啦。我什么都能办到，但是真的不会做饭。.wav'
-    result = transcribe(file_path)
-    print('result is {}'.format(result))
+    file_path = '../tests/你不要让我做饭啦我什么都能办到但是真的不会做饭.wav'
+    with open(file_path, 'rb') as f:
+        byteio = io.BytesIO(f.read())
+        byteio.name = 'ok'
+        result = transcribe(byteio)
+        print('result is {}'.format(result))
